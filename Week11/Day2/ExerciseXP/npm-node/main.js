bc989279961755c9e4edf5cbbf19c8407471257e
Hanna8KH <hannarozen98@gmail.com>
2023-06-23 22:04:51 +0300

    week11 day2 ex xp

---
+++ Week11/Day2/ExerciseXP/npm-node/main.js
@@ -0,0 +1,20 @@
+// Exercise 1
+
+// const a = 5;
+// const b = 10;
+
+// console.log(`Hello you are ${a + b}`);
+
+// Exercise 2
+
+// console.log("Hi! How are you?");
+
+// Exercise 3
+
+const a = 5;
+const b = 10;
+let x;
+const c = 6;
+x = a + b;
+console.log(`the sum of a + b is ${a + b}`);
+console.log(`the sum of x + c is ${x + c}`);
